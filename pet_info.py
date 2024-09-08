d66cc7259e9a9f91a3e2dd28498f99f75f975bfb
danielguidini <danielguidini2002@gmail.com>
2024-09-07 23:16:37 -0300

    fix: corrigindo alguns possiveis loops

--- pet_info.py
+++ pet_info.py
@@ -1,12 +1,16 @@
 import json
+
 def coletar_informacoes_pet():
     print("Por favor, insira as informações sobre seu pet.")
 
     # Coleta do nome do pet
-    while not nome.strip():
-        print("Por favor, insira um nome válido para o pet.")
+    while True:
         nome = input("Nome do pet: ")
-    
+        if nome.strip():
+            break
+        else:
+            print("Por favor, insira um nome válido para o pet.")
+
     # Coleta do tipo do pet, garante que o pet seja definido por seu tipo
     print("Selecione o tipo do pet:")
     print("1. Canidéos")
@@ -14,19 +18,17 @@ def coletar_informacoes_pet():
     print("3. Aves")
     print("4. Répteis & Anfíbios")
 
+    tipo_options = {
+        "1": "Canidéos",
+        "2": "Felinos",
+        "3": "Aves",
+        "4": "Répteis & Anfíbios"
+    }
 
     while True:
         tipo = input("Tipo do pet (digite o número da opção): ")
-        if tipo in ["1", "2", "3", "4"]:
-            if tipo == "1":
-                tipo = "Canidéos"
-            elif tipo == "2":
-                tipo = "Felinos"
-            elif tipo == "3":
-                tipo = "Aves"
-            elif tipo == "4":
-                tipo = "Répteis & Anfíbios"
-
+        if tipo in tipo_options:
+            tipo = tipo_options[tipo]
             break
         else:
             print("Por favor, insira um número válido para o tipo do pet.")
@@ -62,15 +64,18 @@ def coletar_informacoes_pet():
 
     # Armazenar as informações do pet em um dicionário
     pet_info = {
-            "nome": nome,
-            "tipo": tipo,
-            "idade": idade,
-            "peso": peso
-        }
+        "nome": nome,
+        "tipo": tipo,
+        "idade": idade,
+        "peso": peso
+    }
 
-        # Salvar as informações do pet em um arquivo
-    with open("pet_info.json", "w") as arquivo:
+    # Salvar as informações do pet em um arquivo
+    try:
+        with open("pet_info.json", "w") as arquivo:
             json.dump(pet_info, arquivo)
+    except Exception as e:
+        print(f"Erro ao salvar as informações do pet: {e}")
 
 # Chama a função para coletar e exibir as informações do pet
 coletar_informacoes_pet()
